bed3a9fb048d871bc7b00030bc3b71b898d69b2d
Noor <noor.abdulwah@gmail.com>
2020-12-30 19:11:01 -0500

    completed!

--- script.js
+++ script.js
@@ -3,41 +3,26 @@ const buttonInput = document.getElementById('btn');
 const inputs = document.getElementById('inputNum');
 const hexa = document.getElementById('hexColor')
 
-//creates grids
-function createGrid(rows, cols) {
+
+let cell;
+let  createGrid = (rows, cols) => {
     grid.style.setProperty('--grid-rows',rows);
     grid.style.setProperty('--grid-cols',cols);
     for (i=0; i<(rows*cols); i++) {
-        let cell = document.createElement('div');
-        grid.appendChild(cell).className = 'divGrid'
-        
+         cell = document.createElement('div');
+        grid.appendChild(cell).className = `divGrid${i}` 
     }
 }
 createGrid(16,16)
 
 
+grid.addEventListener('click', (e) => {
+    e.target.style.backgroundColor = `${hexa.value}`
+
+})
+
 //button event listener 
-buttonInput.addEventListener('click', () => {
-    grid.innerHTML = ''
+buttonInput.addEventListener('click', () => {    grid.innerHTML = ''
     let values = inputs.value;
     createGrid(values,values)
 })
-
-
-
-function iterate() {
-    const c = Array.from(document.getElementById('grid').childNodes);
-    let p = 0
-    var g  = [...c]
-    if (g) {
-        for(let i=0; i<=g.length-1; i++) {
-            g[i].addEventListener('mouseover', () => {
-                p+=1;
-                console.log(p);
-                
-            })
-        }
-    }
-    
-}
-iterate()
